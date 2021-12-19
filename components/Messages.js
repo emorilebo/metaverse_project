0a21855c11b8d85c30115ab587653458bd8719b6
emorilebo <emorylebo@gmail.com>
2021-12-20 00:19:39 +0100

    ADD Messages

---
+++ components/Messages.js
@@ -0,0 +1,9 @@
+function Messages() {
+  return (
+    <div>
+      <h1>I am the messages</h1>
+    </div>
+  );
+}
+
+export default Messages;
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
 import Header from "../components/Header";
+import Messages from "../components/Messages";
 
 export default function Home() {
-  const { isAuthenticated, logout } = useMoralis();
+  const { isAuthenticated } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
   return (
@@ -15,12 +16,9 @@ export default function Home() {
       </Head>
 
       <div className="max-w-screen-2xl mx-auto">
-        {/* Header */}
         <Header />
-        {/* Messages */}
+        <Messages />
       </div>
-
-      <button onClick={logout}>Logout</button>
     </div>
   );
 }
