b8210f9f71c0232a5244d4e37f559fcd4ab0d676
emorilebo <emorylebo@gmail.com>
2021-12-19 15:11:59 +0100

    UPDATE Login

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 function Login() {
   return (
-    <div>
+    <div className="bg-black relative">
       <h1>Login</h1>
+      <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+        <Image src="/cregtlogo.jpeg" width={200} height={200} />
+
+        <button>Login to the Metaverse</button>
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          src="https://links.papareact.com/55n"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,15 @@ import Head from "next/head";
 import Login from "../components/Login";
 
 export default function Home() {
+  const isAuthenticated = false;
+  if (!isAuthenticated) return <Login />;
   return (
-    <div className="">
+    <div className="h-screen">
       <Head>
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to the metaverse</h1>
-      <Login />
+      <h1>Welcome to the app</h1>
     </div>
   );
 }
