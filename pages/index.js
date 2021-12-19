9b2f9002210f60d95197d93204d3eac20533d531
emorilebo <emorylebo@gmail.com>
2021-12-19 16:48:20 +0100

    UPDATE Authenitcation

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
 
 export default function Home() {
-  const { isAuthenticated } = useMoralis();
-  const isAuthenticated = false;
+  const { isAuthenticated, logout } = useMoralis();
+
   if (!isAuthenticated) return <Login />;
   return (
     <div className="h-screen">
@@ -13,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Welcome to the app</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
