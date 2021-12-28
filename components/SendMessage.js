c58e33db332c108653410e8b543607ba54f38120
emorilebo <emorylebo@gmail.com>
2021-12-28 18:11:26 +0100

    UPDATE SendMessage

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -3,7 +3,7 @@ import { useMoralis } from "react-moralis";
 function SendMessage() {
   const { user, Moralis } = useMoralis();
   return (
-    <form className="flex fixed bottom-10 bg-black opacity-80 w-11/12 px-6 py-4 max-w-2xl">
+    <form className="flex fixed bottom-10 bg-black opacity-80 w-11/12 px-6 py-4 max-w-2xl shadow-xl rounded-full border-4 border-blue-400">
       <input
         className="flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5"
         type="text"
