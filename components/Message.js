65ed6d9cf4618ec5992a252cb1b237f63a5d0f8f
emorilebo <emorylebo@gmail.com>
2021-12-31 10:24:16 +0100

    UPDATE message

--- components/Message.js
+++ components/Message.js
@@ -5,7 +5,7 @@ function Message({ message }) {
 
   const isUserMessage = message.get("ethAddress") === user.get("ethAddress");
   return (
-    <div>
+    <div className="flex space-x-4 p-3 rounded-lg">
       <p>{message.get("message")}</p>
     </div>
   );
