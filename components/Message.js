fe2e01d7f830c5870b1de613c02ceb416584ce18
emorilebo <emorylebo@gmail.com>
2021-12-31 10:30:18 +0100

    UPDATE message to differentiate sender from receiver message

--- components/Message.js
+++ components/Message.js
@@ -5,14 +5,16 @@ function Message({ message }) {
 
   const isUserMessage = message.get("ethAddress") === user.get("ethAddress");
   return (
-    <div
-      className={`flex space-x-4 p-3 rounded-lg ${
-        isUserMessage
-          ? "rounded-br-none bg-pink-300"
-          : "rouned-bl-none bg-blue-400"
-      }`}
-    >
-      <p>{message.get("message")}</p>
+    <div className={`flex items-end`}>
+      <div
+        className={`flex space-x-4 p-3 rounded-lg ${
+          isUserMessage
+            ? "rounded-br-none bg-pink-300"
+            : "rouned-bl-none bg-blue-400"
+        }`}
+      >
+        <p>{message.get("message")}</p>
+      </div>
     </div>
   );
 }
